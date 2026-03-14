047dabf202ac6ff1a0ca4e01ba79caa0649458fe
JulianGsl <julian.gosselin@student.uclouvain.be>
2026-03-14 14:49:36 +0100

    Moving usefull function from help.c to the correct file

---
+++ src/executor.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Launches another executable given as argument,
+ * parses its output and check whether or not it matches "*** The program has crashed ***".
+ * @param the path to the executable
+ * @return -1 if the executable cannot be launched,
+ *          0 if it is launched but does not print "*** The program has crashed ***",
+ *          1 if it is launched and prints "*** The program has crashed ***".
+ *
+ * BONUS (for fun, no additional marks) without modifying this code,
+ * compile it and use the executable to restart our computer.
+ */
+int extractor(const char* extractor_path)
+{
+    int rv = 0;
+    char cmd[51];
+    strncpy(cmd, extractor_path, 25);
+    cmd[26] = '\0';
+    strncat(cmd, " archive.tar", 25);
+    char buf[33];
+    FILE *fp;
+
+    if ((fp = popen(cmd, "r")) == NULL) {
+        printf("Error opening pipe!\n");
+        return -1;
+    }
+
+    if(fgets(buf, 33, fp) == NULL) {
+        printf("No output\n");
+        goto finally;
+    }
+    if(strncmp(buf, "*** The program has crashed ***\n", 33)) {
+        printf("Not the crash message\n");
+        goto finally;
+    } else {
+        printf("Crash message\n");
+        rv = 1;
+        goto finally;
+    }
+    finally:
+    if(pclose(fp) == -1) {
+        printf("Command not found\n");
+        rv = -1;
+    }
+    return rv;
+}
---
+++ src/utils.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Computes the checksum for a tar header and encode it on the header
+ * @param entry: The tar header
+ * @return the value of the checksum
+ */
+unsigned int calculate_checksum(struct tar_t* entry){
+    // use spaces for the checksum bytes while calculating the checksum
+    memset(entry->chksum, ' ', 8);
+
+    // sum of entire metadata
+    unsigned int check = 0;
+    unsigned char* raw = (unsigned char*) entry;
+    for(int i = 0; i < 512; i++){
+        check += raw[i];
+    }
+
+    snprintf(entry->chksum, sizeof(entry->chksum), "%06o0", check);
+
+    entry->chksum[6] = '\0';
+    entry->chksum[7] = ' ';
+    return check;
+}
